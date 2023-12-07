2289eca002379517b0a503caaaac1e599751626c
gyun <akf151568@gmail.com>
2023-12-07 22:52:52 +0900

    board(Register 완성)

--- src/main/java/com/main/toto/controller/BoardController.java
+++ src/main/java/com/main/toto/controller/BoardController.java
@@ -6,11 +6,15 @@ import com.main.toto.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -24,6 +28,8 @@ public class BoardController {
 
     private final BoardService boardService;
 
+
+
     // 후에 카테고리 별로 1개씩 가져와서 보여주는 느낌으로 할 것
     // board service에서 3개 가져옴.
     @GetMapping("/main")
@@ -33,4 +39,35 @@ public class BoardController {
         model.addAttribute("dtoList", dtoList);
         return "/toto/main";
     }
+
+//    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/board/register")
+    public void registerGet(){}
+
+    @ModelAttribute("boardCategories")
+    public BoardCategory[] boardCategories(){
+        return BoardCategory.values();
+    }
+
+    @PostMapping("/board/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        log.info("board POST register .....");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors.......");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/toto/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result" , bno);
+
+        return "redirect:/toto/main";
+    }
+
+
 }
--- src/main/java/com/main/toto/domain/board/BoardCategory.java
+++ src/main/java/com/main/toto/domain/board/BoardCategory.java
@@ -1,9 +1,24 @@
 package com.main.toto.domain.board;
 
+import lombok.Getter;
+
 /**
  * 패션, 음식, 여행, 뷰티, 건강, 스포츠, 취미, 게임, 영화, 음악, 책, 동물, 기타
  */
 
 public enum BoardCategory {
-    FASHION, FOOD, TRAVEL, BEAUTY, HEALTH, SPORTS, HOBBY, GAME, MOVIE, MUSIC, BOOK, ANIMAL, ETC
+    FASHION("패션"), FOOD("음식"),
+    TRAVEL("여행"), BEAUTY("뷰티"),
+    HEALTH("건강"), SPORTS("스포츠"),
+    HOBBY("취미"), GAME("게임"),
+    MOVIE("영화"), MUSIC("음악"),
+    BOOK("책"), ANIMAL("동물"), ETC("기타");
+
+    @Getter
+    private final String description;
+
+    BoardCategory(String description) {
+        this.description = description;
+    }
+
 }
